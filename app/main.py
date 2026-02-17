898defde07737e30eb1c7d780cd6b9fa1cb7e7f1
Nabrikim <brayosanta@gmail.com>
2026-02-17 00:56:19 -0800

    add changes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Depends
 from fastapi.middleware.cors import CORSMiddleware
 from . import models,schemas
-from .database import engine
+from .database import engine,get_db
 from .routers import posts,user,auth,votes
 from .config import settings
 from .database import engine
-from sqlalchemy import text
+from sqlalchemy.orm import Session
+from sqlalchemy import text,inspect
+
 
 
 with engine.connect() as conn:
@@ -28,6 +30,13 @@ app.include_router(posts.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(votes.router)
+
+
+@app.get("/verify-db")
+def verify_db(db: Session = Depends(get_db)):
+    inspector = inspect(engine)
+    tables = inspector.get_table_names()
+    return {"existing_tables": tables}
 
 
 
